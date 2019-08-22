e93ce1bb78d0e4b104e50ea0381435acb752e3bb
Dyanna Tolman <dyannast@gmail.com>
2019-08-22 16:59:45 -0700

    Updates

---
+++ precache-manifest.5ffe13d5ae121c308a12732a2141fe3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d610eb34a1eb053b9d1812273455ca7e",
+    "url": "/deetolman/portfolio-dyanna/index.html"
+  },
+  {
+    "revision": "6d1343a27f83c14e05bf",
+    "url": "/deetolman/portfolio-dyanna/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "953480075ba591db4f2f",
+    "url": "/deetolman/portfolio-dyanna/static/js/2.129fcd71.chunk.js"
+  },
+  {
+    "revision": "6d1343a27f83c14e05bf",
+    "url": "/deetolman/portfolio-dyanna/static/js/main.8641f68d.chunk.js"
+  },
+  {
+    "revision": "128eaf746ef43db69c27",
+    "url": "/deetolman/portfolio-dyanna/static/js/runtime~main.2918e433.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deetolman/portfolio-dyanna/static/media/logo.5d5d9eef.svg"
+  }
+]);
